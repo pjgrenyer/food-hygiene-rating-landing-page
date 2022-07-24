7c6ddb191d58bef8aeeb2d9bf9f98fa4b97c003f
Paul Grenyer <paul.grenyer@gmail.com>
2022-07-24 14:44:19 +0100

    feat: tried react - it didn't work

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <p>Kiss me!</p>
+    <BrowserRouter>
+        <Route path = "/">
+        
+        </Route>
+    </BrowserRouter>
+    
+    </>
+  );
+}
+
+export default App;
